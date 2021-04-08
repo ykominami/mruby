3968abd766732cffbc9d9e63acedc28eceae3865
yasuo kominami <ykominami@gmail.com>
2021-04-08 14:22:13 +0900

    make being able to invoke windows program in cygwin

--- build_config_host_all_arm.rb
+++ build_config_host_all_arm.rb
@@ -1,4 +1,5 @@
 MRuby::Build.new do |conf|
+  # load specific toolchain settings
 
   # Gets set by the VS command prompts.
   if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
@@ -7,8 +8,7 @@ MRuby::Build.new do |conf|
     toolchain :gcc
   end
 
-  # Turn on `enable_debug` for better debugging
-  # enable_debug
+  enable_debug
 
   # Use mrbgems
   # conf.gem 'examples/mrbgems/ruby_extension_example'
@@ -28,7 +28,7 @@ MRuby::Build.new do |conf|
   #   cc.command = ENV['CC'] || 'gcc'
   #   cc.flags = [ENV['CFLAGS'] || %w()]
   #   cc.include_paths = ["#{root}/include"]
-  #   cc.defines = %w()
+  #   cc.defines = %w(DISABLE_GEMS)
   #   cc.option_include_path = '-I%s'
   #   cc.option_define = '-D%s'
   #   cc.compile_options = "%{flags} -MMD -o %{outfile} -c %{infile}"
@@ -84,6 +84,7 @@ MRuby::Build.new do |conf|
   # conf.enable_bintest
 end
 
+
 MRuby::Build.new('host-debug') do |conf|
   # load specific toolchain settings
 
@@ -96,7 +97,7 @@ MRuby::Build.new('host-debug') do |conf|
 
   enable_debug
 
-  # include the default GEMs
+  # include the default GEM
   conf.gembox 'default'
 
   # C compiler settings
@@ -108,6 +109,7 @@ MRuby::Build.new('host-debug') do |conf|
   # bintest
   # conf.enable_bintest
 end
+
 
 MRuby::Build.new('test') do |conf|
   # Gets set by the VS command prompts.
@@ -124,17 +126,17 @@ MRuby::Build.new('test') do |conf|
   conf.gembox 'default'
 end
 
-#MRuby::Build.new('bench') do |conf|
-#  # Gets set by the VS command prompts.
-#  if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
-#    toolchain :visualcpp
-#  else
-#    toolchain :gcc
-#    conf.cc.flags << '-O3'
-#  end
-#
-#  conf.gembox 'default'
-#end
+MRuby::Build.new('bench') do |conf|
+  # Gets set by the VS command prompts.
+  if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
+    toolchain :visualcpp
+  else
+    toolchain :gcc
+    conf.cc.flags << '-O3'
+  end
+
+  conf.gembox 'default'
+end
 
 # Define cross build settings
 # MRuby::CrossBuild.new('32bit') do |conf|
@@ -148,6 +150,7 @@ end
 #   conf.gem 'examples/mrbgems/c_and_ruby_extension_example'
 #
 #   conf.test_runner.command = 'env'
+#
 # end
 
 load './build_config_arm.rb' if File.exist?('./build_config_arm.rb')
