d273f7443e7d8ed882336fdcead5c1fc17e6d25a
yasuo kominami <ykominami@gmail.com>
2021-04-08 14:22:13 +0900

    make being able to invoke windows program in cygwin

--- build_config_host_all_arm.rb
+++ build_config_host_all_arm.rb
@@ -8,8 +8,7 @@ MRuby::Build.new do |conf|
     toolchain :gcc
   end
 
-  # Turn on `enable_debug` for better debugging
-  # enable_debug
+  enable_debug
 
   # Use mrbgems
   # conf.gem 'examples/mrbgems/ruby_extension_example'
@@ -85,6 +84,7 @@ MRuby::Build.new do |conf|
   # conf.enable_bintest
 end
 
+
 MRuby::Build.new('host-debug') do |conf|
   # load specific toolchain settings
 
@@ -97,7 +97,7 @@ MRuby::Build.new('host-debug') do |conf|
 
   enable_debug
 
-  # include the default GEMs
+  # include the default GEM
   conf.gembox 'default'
 
   # C compiler settings
@@ -109,6 +109,7 @@ MRuby::Build.new('host-debug') do |conf|
   # bintest
   # conf.enable_bintest
 end
+
 
 MRuby::Build.new('test') do |conf|
   # Gets set by the VS command prompts.
@@ -125,17 +126,17 @@ MRuby::Build.new('test') do |conf|
   conf.gembox 'default'
 end
 
-#MRuby::Build.new('bench') do |conf|
-#  # Gets set by the VS command prompts.
-#  if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
-#    toolchain :visualcpp
-#  else
-#    toolchain :gcc
-#    conf.cc.flags << '-O3'
-#  end
-#
-#  conf.gembox 'default'
-#end
+MRuby::Build.new('bench') do |conf|
+  # Gets set by the VS command prompts.
+  if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
+    toolchain :visualcpp
+  else
+    toolchain :gcc
+    conf.cc.flags << '-O3'
+  end
+
+  conf.gembox 'default'
+end
 
 # Define cross build settings
 # MRuby::CrossBuild.new('32bit') do |conf|
@@ -149,6 +150,7 @@ end
 #   conf.gem 'examples/mrbgems/c_and_ruby_extension_example'
 #
 #   conf.test_runner.command = 'env'
+#
 # end
 
 load './build_config_arm.rb' if File.exist?('./build_config_arm.rb')
